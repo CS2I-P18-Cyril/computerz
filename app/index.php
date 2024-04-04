0ceed42e114be6fa274a3c5578902d5087b901d3
Uti Lisateur <debian@debian.local>
2024-04-04 14:52:57 +0200

    Ajout index.php

--- app/index.php
+++ app/index.php
@@ -1,34 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML Base Template</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-</head>
+<?php include "includes/head.php" ?>
+
+
 
 <body>
-    <div class="container p-3">
-       <h1><i class="bi bi-tools"></i> Under construction</h1>
+    <div class="container-fluid ">
+        <!-- Menu (Navbar Bootstrap) -->
+        <?php include "includes/navigation.php" ?>
 
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Suscipit distinctio, pariatur facilis nesciunt rem natus. 
-            Aut eligendi sequi iure quisquam cumque quibusdam tenetur vel, deleniti recusandae quae fuga, 
-            ratione ea adipisci praesentium. 
-            Quod earum asperiores error, voluptate accusantium neque repudiandae quaerat. 
-            Quisquam, voluptatibus aliquam laboriosam corrupti sed quibusdam tenetur quo esse maiores minus, 
-            consequatur quidem eveniet ! 
-        </p>
-    </div>
 
-    <!-- JS -->
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <script src="js/scripts.js"></script>
+
+
+
+        <div class="p-3 mb-2 bg-light text-secondary-emphasis  ">
+            <h1 class=" p-3 text-center"> Bienvenue chez <b>Computerz</b> ! </i></h1>
+        </div>
+
+        <p><img src="images/promo.jpg" class="img-fluid" alt="..."></p>
+        <div class="row">
+            <div class="col-md-4">
+            <h1 class="display-4 mb-3"><b>Notre boutique</b></h1>
+                <div class="border mb-3"></div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem similique, officia ab reiciendis temporibus eveniet porro minima odit fuga quibusdam incidunt inventore quis vero, nisi possimus, ipsum earum aliquid? Laudantium maiores cumque dolore temporibus quam facere, reiciendis impedit eligendi dolorum. Quos, repellat minus. Nihil, sequi itaque velit odit ducimus, magnam consequatur iure ipsa quo minus tenetur esse quia quod dolore vero tempore perferendis quos odio repellendus laboriosam nulla. Voluptatem voluptatibus, sunt perspiciatis cumque sint, doloremque earum labore explicabo, provident assumenda aspernatur dicta in at maxime ut officiis mollitia aliquid et autem fugiat consectetur. Minus nulla, praesentium laudantium hic, dignissimos maiores, sapiente asperiores possimus saepe maxime rem accusamus! Quo minus molestiae quaerat porro dolorum ratione vero molestias culpa ab doloribus, animi quia qui sit commodi modi id nihil unde voluptatem placeat velit aliquam soluta ut. Voluptatem, non ducimus!</p></div>
+
+            <div class="col-md-4">
+                <h1 class="display-4 mb-3"><b>Nos services</b></h1>
+
+                <div class="border mb-3"></div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta exercitationem culpa a totam expedita ullam porro, beatae provident pariatur sunt vero esse excepturi unde quasi soluta dolore laborum libero. Tenetur, ratione porro minus sequi, esse eveniet accusantium odio ex sunt exercitationem, perferendis quidem! Velit modi architecto quam officia maxime veritatis expedita reiciendis animi reprehenderit fuga at quaerat dolorum perspiciatis, praesentium deleniti tempore eveniet est doloremque sapiente cumque ipsum quod, nesciunt accusamus vitae? Nam beatae aut, voluptatem natus doloribus amet, commodi ex magni temporibus perferendis dolorum eaque placeat pariatur itaque corrupti libero facere enim necessitatibus. Ad esse fugiat porro quisquam nesciunt ipsa modi explicabo mollitia iusto soluta nihil cupiditate ex quis distinctio enim, earum animi, fuga non sapiente ratione error laborum optio rem facilis? Similique harum magni quos consequatur aperiam excepturi!
+                </p>
+                <p>
+                    <a href="services.php"><b>></b>En savoir plus</a>
+                </p>
+            </div>
+
+
+            <div class="col-md-4">
+            <h1 class="display-4 mb-3"><b>Notre catalogue</b></h1>
+                
+
+                <div class="border mb-3"></div>
+                <p><b>Notre catalogue d'ordinateurs vous propose plus de 300 modèles (neufs et occasions)</b></p>
+                <p><img src="images/dell-xps-13-7390.jpg" class="img-fluid" alt="..."></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quasi totam iste deleniti quaerat velit laboriosam cum, beatae, eius, doloremque nemo libero? Voluptas odit velit quos magnam, ab eveniet quasi corrupti itaque libero doloribus. Veritatis impedit recusandae quas consequatur, animi nihil, voluptas illum, consectetur dolores cumque dolore ullam ipsa maiores!
+                </p>
+                <p><a href="catalogue.php"><b>></b>Consultez le catalogue Ordinateurs</a></p>
+            </div>
+        </div>
+
+
+
 </body>
+
+<?php include "includes/footer.php" ?>
+
 </html>
