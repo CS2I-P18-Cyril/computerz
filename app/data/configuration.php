705deff0b44ff788776e24d9052bfa6bf1fbc91c
Uti Lisateur <debian@debian.local>
2024-04-04 16:01:50 +0200

    Ajout page Catalogue et fin du site

--- app/data/configuration.php
+++ app/data/configuration.php
@@ -1,18 +1,30 @@
 <?php
-	// Fichier data/configuration.php
+// Fichier data/configuration.php
 
-	// Debug
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+// Debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-	// Configuration de la base de données
-	$host =      'pgdb';
-	$database =  'appdb';
-	$port =      5432;
-	$user =      'postgres';
-	$password =  'letmein';
+// Configuration de la base de données
+$host =      'pgdb';
+$database =  'appdb';
+$port =      5432;
+$user =      'postgres';
+$password =  'letmein';
 
-	// TODO
-	// Connexion à la base de données
-	// ...
+// TODO
+// Connexion à la base de données
+// Version finale du DSN :
+$dsn = "pgsql:host=$host; port=$port; dbname=$database;";
+
+// Ouvrir la connexion (=> créer un objet PDO)
+$pdo = new PDO($dsn, $user, $password);
+
+
+// Définir le SEARCH_Path
+$sql = "SET SEARCH_PATH TO computerz";
+$requete = $pdo->prepare($sql);
+$requete->execute();
+
+?>
--- app/index.php
+++ app/index.php
@@ -17,7 +17,8 @@
         <div class="p-3 mb-2 bg-light text-secondary-emphasis  ">
             <h1 class=" p-4 text-center"> Bienvenue chez <b>Computerz</b> ! </i></h1>
         </div>
-
+    </div>
+    <div class ="container">
         <p><img src="images/promo.jpg" class="img-fluid" alt="..."></p>
         <div class="row">
             <div class="col-md-4">
